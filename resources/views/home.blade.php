1493b058b7c05d606225c33989754574db357e45
Naimah Hussain <naimahhussain@Naimahs-MacBook-Air.local>
2026-05-15 17:33:34 +0300

    Created custom home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Naimah Website</title>
+</head>
+<body>
+
+    <h1>Welcome to My Laravel Website 🎀</h1>
+
+    <p>I built this website using Laravel.</p>
+
+</body>
+</html>
