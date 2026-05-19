f5f39d857c346fc2093d109db599bb373127c94f
Naimah Hussain <naimahhussain@Naimahs-MacBook-Air.local>
2026-05-19 18:05:43 +0300

    Implemented SB Admin template

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
-
+use App\Http\Controllers\Admin\AdminController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FrontController;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [FrontController::class, 'index']);
+Route::get('/admin', [AdminController::class, 'index']);
+
+Route::get('/admin/dashboard', function () {
+    return view('admin.dashboard');
+})->name('admin.dashboard');
